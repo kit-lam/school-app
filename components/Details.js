4f3620f4efe615172bf2ce3bec5f3c561e28b638
Emilee Lam <emileelam@hotmail.com>
2019-06-16 12:01:03 +0100

    handle no sat results, and display loading screens

--- components/Details.js
+++ components/Details.js
@@ -4,42 +4,46 @@ import { getSatScores } from "../data";
 
 export default class Details extends Component {
   state = {
-    isLoading: true,
-    error: null
+    isLoading: true
   };
 
   componentDidMount() {
-    console.log("hello", this.props.navigation.getParam("dbn"));
-
     const dbn = this.props.navigation.getParam("dbn");
 
     getSatScores(dbn).then(response =>
       this.setState({
-        response
+        response,
+        isLoading: false
       })
     );
   }
 
   render() {
-    console.log("details sat", this.state.response);
+    console.log(this.state.response && this.state.response.length);
     return (
-      <View>
-        <Text>HI</Text>
-        <FlatList
-          data={this.state.response}
-          renderItem={({ item }) => (
-            <View>
-              <Text style={styles.welcome}>{item.school_name}</Text>
-              <Text>SATS</Text>
-              <Text>Writing Average Score: {item.sat_writing_avg_score}</Text>
-              <Text>Math Average Score: {item.sat_math_avg_score}</Text>
-              <Text>
-                Critical Reading Average Score:{" "}
-                {item.sat_critical_reading_avg_score}
-              </Text>
-            </View>
-          )}
-        />
+      <View style={styles.container}>
+        {this.state.isLoading ? (
+          <Text>Loading....</Text>
+        ) : this.state.response && this.state.response.length > 0 ? (
+          <FlatList
+            data={this.state.response}
+            renderItem={({ item }) => (
+              <View>
+                <Text style={styles.welcome}>{item.school_name}</Text>
+                <Text>SATS</Text>
+                <Text>Writing Average Score: {item.sat_writing_avg_score}</Text>
+                <Text>Math Average Score: {item.sat_math_avg_score}</Text>
+                <Text>
+                  Critical Reading Average Score:
+                  {item.sat_critical_reading_avg_score}
+                </Text>
+              </View>
+            )}
+            keyExtractor={item => item.dbn}
+          />
+        ) : (
+          <Text>No SAT results</Text>
+        )}
       </View>
     );
   }
--- components/Home.js
+++ components/Home.js
@@ -14,19 +14,18 @@ import {
   FlatList,
   TouchableOpacity
 } from "react-native";
-import { createAppContainer } from "react-navigation";
 import { getSchools } from "../data";
 
 export default class Home extends Component {
   state = {
-    isLoading: true,
-    error: null
+    isLoading: true
   };
 
   componentDidMount() {
     getSchools().then(data =>
       this.setState({
-        data
+        data,
+        isLoading: false
       })
     );
   }
@@ -34,26 +33,29 @@ export default class Home extends Component {
   render() {
     const { isLoading, data } = this.state;
 
-    console.log(this.state);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>NYC Schools</Text>
         <Text style={styles.instructions}>
           Click on the links for more information
         </Text>
-
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => {
-                this.props.navigation.navigate("Details", { dbn: item.dbn });
-              }}
-            >
-              <Text style={styles.welcome}>{item.school_name}</Text>
-            </TouchableOpacity>
-          )}
-        />
+        {isLoading ? (
+          <Text>Loading Schools...</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate("Details", { dbn: item.dbn });
+                }}
+              >
+                <Text style={styles.welcome}>{item.school_name}</Text>
+              </TouchableOpacity>
+            )}
+            keyExtractor={item => item.dbn}
+          />
+        )}
       </View>
     );
   }
